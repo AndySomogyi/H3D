8fac1b2c4b6d3af31932945e8371707c922e9a02
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2006-04-07 06:45:57 +0000

    made a copy

---
+++ branches/HaptikH3D/src/FieldTemplates.h
@@ -0,0 +1,90 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __FIELDTEMPLATES_H__
+#define __FIELDTEMPLATES_H__
+
+#include <string>
+#include <set>
+#include <vector>
+
+#include "SField.h"
+#include "MField.h"
+#include "TypedField.h"
+
+using namespace std;
+
+namespace H3D {
+
+  H3D_VALUE_EXCEPTION( string, InvalidNodeType );
+
+  /// Template to make sure that the Node that is set in a SFNode is
+  /// of a specified Node type.
+  /// \param RefCountedTyoe The expected RefcountedClass type. 
+  /// \param BaseRefCountType The base field type.
+  /// \ingroup FieldTemplateModifiers
+  template< class RefCountedType, class BaseRefCountType >
+  class TypedRefCountField: public BaseRefCountType {
+  protected:
+    /// We check that the type of the Node is of the correct type.
+    void onAdd( typename BaseRefCountType::value_type n) {
+      if( !dynamic_cast< RefCountedType * >( n ) ) {
+        stringstream s;
+        s << "Expecting " << typeid( RefCountedType ).name();
+        throw InvalidNodeType( typeid( n ).name(),
+                               s.str(),
+                               H3D_FULL_LOCATION );
+  
+      } else {
+        BaseRefCountType::onAdd(  n );
+      }
+    }
+  public:
+    /// Get the value casted to the RefCountType.
+    virtual RefCountedType *getValue() {
+      return static_cast< RefCountedType * >( BaseRefCountType::getValue() );
+    }
+    
+  };
+
+  /// The AutoUpdate field is a template to force the BaseField to update 
+  /// itself as soon as an event is received. Normally the value of the 
+  /// field has to be requested in order for an update to take place.
+  /// \ingroup FieldTemplateModifiers
+  template< class BaseFieldType >
+  struct AutoUpdate: public BaseFieldType {
+    /// Receive an event from another field. We call the upToDate() 
+    /// function to update the field.
+    ///
+    virtual void propagateEvent( Field::Event e ) {
+      BaseFieldType::propagateEvent( e );
+      this->upToDate();
+    }
+  };
+}
+
+#endif
+
---
+++ branches/HaptikH3D/src/X3D.cpp
@@ -0,0 +1,197 @@
+//////////////////////////////////////////////////////////////////////////////
+//    H3D API.   Copyright 2004, Danel Evestedt, Mark Dixon
+//    All Rights Reserved
+//
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+#include "X3D.h"
+#include "X3DSAX2Handlers.h"
+#include "IStreamInputSource.h"
+#include "ResourceResolver.h"
+#include <sstream>
+
+using namespace H3D;
+
+SAX2XMLReader* X3D::getNewXMLParser() {
+  SAX2XMLReader::ValSchemes    valScheme    = SAX2XMLReader::Val_Never;
+  bool                         doNamespaces = true;
+  bool                         doSchema = false;
+  bool                         schemaFullChecking = false;
+  //bool                         doList = false;
+  //bool                         errorOccurred = false;
+  bool                         namespacePrefixes = false;
+  //bool                         recognizeNEL = false;
+  char                         localeStr[64];
+  memset(localeStr, 0, sizeof localeStr);
+  SAX2XMLReader* parser = XMLReaderFactory::createXMLReader();
+  parser->setFeature(XMLUni::fgSAX2CoreNameSpaces, doNamespaces);
+  parser->setFeature(XMLUni::fgXercesSchema, doSchema);
+  parser->setFeature(XMLUni::fgXercesSchemaFullChecking, 
+                     schemaFullChecking);
+  parser->setFeature(XMLUni::fgSAX2CoreNameSpacePrefixes, 
+                     namespacePrefixes);
+  if (valScheme == SAX2XMLReader::Val_Auto) {
+    parser->setFeature(XMLUni::fgSAX2CoreValidation, true);
+    parser->setFeature(XMLUni::fgXercesDynamic, true);
+  }
+  if (valScheme == SAX2XMLReader::Val_Never) {
+    parser->setFeature(XMLUni::fgSAX2CoreValidation, false);
+  }
+  if (valScheme == SAX2XMLReader::Val_Always) {
+    parser->setFeature(XMLUni::fgSAX2CoreValidation, true);
+    parser->setFeature(XMLUni::fgXercesDynamic, false);
+  }
+  return parser;
+}
+
+Group* X3D::createX3DFromString( const string &str,
+                                 DEFNodes *dn,
+				 DEFNodes *exported_nodes,
+				 PrototypeVector *prototypes  ) {
+  Group *g = new Group;
+  AutoRef< Node > n = createX3DNodeFromString( str, dn,
+					       exported_nodes, 
+					       prototypes  );
+  if( n.get() )
+    g->children->push_back( n.get() );
+  return g;
+}
+
+Group* X3D::createX3DFromURL( const string &url,
+                              DEFNodes *dn,
+			      DEFNodes *exported_nodes,
+			      PrototypeVector *prototypes ) {
+  Group *g = new Group;
+  AutoRef< Node > n = createX3DNodeFromURL( url, dn, exported_nodes, 
+					    prototypes );
+  if( n.get() )
+    g->children->push_back( n.get() );
+  return g;
+}
+
+Group* X3D::createX3DFromStream( istream &is, 
+                                 DEFNodes *dn,
+				 DEFNodes *exported_nodes,
+				 PrototypeVector *prototypes,
+                                 const XMLCh *const system_id ) {
+  Group *g = new Group;
+  AutoRef< Node > n = createX3DNodeFromStream( is, dn, exported_nodes, 
+					       prototypes,system_id );
+  if( n.get() )
+    g->children->push_back( n.get() );
+  return g;
+}
+
+
+AutoRef< Node > X3D::createX3DNodeFromString( const string &str,
+                                              DEFNodes *dn,
+					      DEFNodes *exported_nodes,
+					      PrototypeVector *prototypes ) {
+  auto_ptr< SAX2XMLReader > parser( getNewXMLParser() );
+  X3DSAX2Handlers handler( dn, exported_nodes, prototypes );
+  stringstream s;
+  s << str;
+  parser->setContentHandler(&handler);
+  parser->setErrorHandler(&handler); 
+  parser->parse( IStreamInputSource( s, (const XMLCh*)L"<string input>" ) );
+  return handler.getResultingNode();
+}
+
+AutoRef< Node > X3D::createX3DNodeFromURL( const string &url,
+                                           DEFNodes *dn,
+					   DEFNodes *exported_nodes,
+					   PrototypeVector *prototypes ) {
+  auto_ptr< SAX2XMLReader > parser( getNewXMLParser() );
+  X3DSAX2Handlers handler( dn, exported_nodes, prototypes );
+  parser->setContentHandler(&handler);
+  parser->setErrorHandler(&handler); 
+  
+  URNResolver *urn_resolver = ResourceResolver::getURNResolver();
+  string urn = url;
+  if( urn_resolver ) urn = urn_resolver->resolveURN( urn );
+  string::size_type pos = urn.find_last_of( "/\\" );
+  string path = urn.substr( 0, pos + 1 );
+  string old_base = ResourceResolver::getBaseURL();
+
+  string resolved_url = ResourceResolver::resolveURLAsFile( url );
+  ResourceResolver::setBaseURL( path ); 
+
+  if( resolved_url == "" )
+    parser->parse( url.c_str() );
+  else {
+    XERCES_CPP_NAMESPACE_USE
+    ifstream is( resolved_url.c_str() );
+    XMLCh *url_ch = new XMLCh[ url.size() + 1 ];
+    for( unsigned int i = 0; i < url.size(); i++ ) {
+      url_ch[i] = url[i];
+    }
+    url_ch[ url.size() ] = '\0'; 
+    parser->parse( IStreamInputSource( is, url_ch ) );
+    delete url_ch;
+	is.close();
+  }
+  ResourceResolver::setBaseURL( old_base );
+  return handler.getResultingNode();
+}
+
+AutoRef< Node > X3D::createX3DNodeFromStream( istream &is, 
+                                              DEFNodes *dn,
+					      DEFNodes *exported_nodes,
+					      PrototypeVector *prototypes,
+                                              const XMLCh *const system_id ) {
+  auto_ptr< SAX2XMLReader > parser( getNewXMLParser() );
+  X3DSAX2Handlers handler( dn, exported_nodes, prototypes );
+  parser->setContentHandler(&handler);
+  parser->setErrorHandler(&handler); 
+  parser->parse( IStreamInputSource( is, system_id ) );
+  return handler.getResultingNode();
+}
+
+void X3D::writeNodeAsX3D( ostream& os, 
+                          Node *node, 
+                          const string& container_field ) {
+  if( !node ) {
+    return;
+  }
+  H3DNodeDatabase *db = H3DNodeDatabase::lookupTypeId( typeid( *node ) );
+  string node_name = node->getTypeName();    
+
+  os << "<" << node_name << " containerField=\"" 
+     << container_field << "\" ";
+    
+  vector< pair< string, SFNode * > > sf_nodes;
+  vector< pair< string, MFNode * > > mf_nodes;
+
+  for( H3DNodeDatabase::FieldDBConstIterator i = db->fieldDBBegin();
+       i != db->fieldDBEnd(); i++ ) {
+    Field *f = node->getField( *i );
+    Field::AccessType access_type = f->getAccessType();
+    if( access_type != Field::INPUT_ONLY &&
+        access_type != Field::OUTPUT_ONLY ) {
+      if( MFNode *mf_node = dynamic_cast< MFNode * >( f ) ) {
+        mf_nodes.push_back( make_pair( f->getName(), mf_node ) );
+      } else if( SFNode *sf_node = dynamic_cast< SFNode * >( f ) ) {
+        sf_nodes.push_back( make_pair( f->getName(), sf_node ) );
+      } else if( ParsableField *p_field = 
+                 dynamic_cast< ParsableField * >( f ) ){
+        os << f->getName() << "=\'" << p_field->getValueAsString() << "\' ";
+      }
+    }
+  }
+
+  os << ">" << endl;
+
+  for( unsigned int i = 0; i < sf_nodes.size(); i++ ) {
+    X3D::writeNodeAsX3D( os, sf_nodes[i].second->getValue(), sf_nodes[i].first );
+  }
+
+  for( unsigned int i = 0; i < mf_nodes.size(); i++ ) {
+    for( MFNode::const_iterator n = mf_nodes[i].second->begin();
+         n != mf_nodes[i].second->end(); n++ ) {
+      X3D::writeNodeAsX3D( os, *n, mf_nodes[i].first );
+    }
+  }
+  
+  os << "</" << node_name << ">" << endl;
+}
