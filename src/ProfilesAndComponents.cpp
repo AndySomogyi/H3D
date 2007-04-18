d0bd16e9c9619d5e3d03e3b8cbd9ab3545ef01f0
markus <markus@610dfd11-2700-0410-9b68-9ae28eac5983>
2007-04-18 07:48:08 +0000

    Added a ! to get correct behaviour in an if-statement.

--- src/ProfilesAndComponents.cpp
+++ src/ProfilesAndComponents.cpp
@@ -260,7 +260,7 @@ bool ProfilesAndComponents::setProfileInternal( string profile,
                i != temp_profile.component_names.end();
                i++ ) {
             int place;
-            if( findComponent( components_used, (*i).first, place ) ) {
+            if( !findComponent( components_used, (*i).first, place ) ) {
               stringstream stm;
               stm << (*i).second;
               if( components_used[ place ].conformance_level < (*i).second ) {
