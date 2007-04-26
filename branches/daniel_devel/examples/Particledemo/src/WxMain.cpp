d5652962f35593185a6293eb1c4a8d12cd54ade2
ram <ram@610dfd11-2700-0410-9b68-9ae28eac5983>
2007-04-26 08:10:51 +0000

    Particle System example

---
+++ branches/daniel_devel/examples/Particledemo/src/WxMain.cpp
@@ -0,0 +1,141 @@
+// ---------------------------------------------------------------------------
+//  Includes
+// ---------------------------------------------------------------------------
+//#pragma once
+//#include "header.h"
+#include "H3DWxFrame.h"
+#include "H3DWxWidgetsWindow.h"
+//#include <fstream>
+//#include "X3D.h"
+//#include <string.h>
+
+//#include <iostream>
+
+//#include "X3DSAX2Handlers.h"
+//#include "GLUTWindow.h"
+#include "VrmlParser.h"
+#include "Group.h"
+#include "Transform.h"
+#include "Scene.h"
+#include "KeySensor.h"
+#include "MouseSensor.h"
+#ifndef MACOSX
+#include "SpaceWareSensor.h"
+#endif
+//#include "DEFNodes.h"
+//#include "Viewpoint.h"
+#include "DeviceInfo.h"
+#include "INIFile.h"
+#include "ResourceResolver.h"
+#include "PythonScript.h"
+
+#include <wx/cmdline.h>
+
+// ---------------------------------------------------------------------------
+//  Required classes and definitions
+// ---------------------------------------------------------------------------
+using namespace std;
+using namespace H3D;
+
+
+H3D_API_EXCEPTION( QuitAPIException );
+
+class QuitAPIField: public AutoUpdate< SFString > {
+  virtual void update() {
+    string s = static_cast< SFString * >(routes_in[0])->getValue();
+    if( s[0] == 27 ) { 
+      throw QuitAPIException();
+    }
+  }
+};
+
+const wxCmdLineEntryDesc gCmdLineDesc[] = 
+  {
+    { wxCMD_LINE_PARAM, NULL, NULL, wxT("File to load"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL}, 
+    { wxCMD_LINE_NONE, NULL, NULL, NULL, wxCMD_LINE_VAL_NONE, 0} };
+
+// Define a new application type
+class MyApp: public wxApp
+{
+public:
+  virtual bool OnInit();
+
+  virtual void OnIdle(wxIdleEvent& event);
+  virtual bool OnExceptionInMainLoop();
+  virtual void OnInitCmdLine(wxCmdLineParser& parser) {
+    parser.SetDesc (gCmdLineDesc); 
+  }
+
+  virtual bool OnCmdLineParsed(wxCmdLineParser& parser) {
+    for (int i = 0; i < parser.GetParamCount(); i++) {
+      cmd_line_filename = parser.GetParam(i);
+    }
+
+    return true;
+  }
+protected:
+  wxString cmd_line_filename;
+
+  DECLARE_EVENT_TABLE()
+};
+
+BEGIN_EVENT_TABLE(MyApp, wxApp)
+  EVT_IDLE(MyApp::OnIdle)
+END_EVENT_TABLE()
+
+void MyApp::OnIdle(wxIdleEvent& event) {
+  for( set< Scene * >::iterator i = Scene::scenes.begin();
+    i != Scene::scenes.end();
+    i++ ) {
+    if( (*i)->isActive() )
+      (*i)->idle();
+  }
+  wxApp::OnIdle(event);
+}
+
+IMPLEMENT_APP(MyApp)
+
+bool MyApp::OnExceptionInMainLoop() {
+  try {
+    throw;
+  }
+  catch (const QuitAPIException &) {
+    return false;
+  }
+  catch (const Exception::H3DException &e) {
+     wxMessageBox(e.message.c_str(), "Error", wxOK | wxICON_EXCLAMATION);
+    return false;
+  }
+  wxApp::OnExceptionInMainLoop();  
+}
+
+bool MyApp::OnInit()
+{
+  // call default behaviour (mandatory)
+  if (!wxApp::OnInit())
+    return false;
+
+  SetVendorName(_T("SenseGraphics AB"));
+  SetAppName(_T("Particle Systems Demo"));
+
+  //initializeH3D();
+
+  // create a window to display
+  H3DWxFrame *theWxFrame = new H3DWxFrame(NULL, wxID_ANY, "Particle Systems - Demo", wxDefaultPosition, wxSize(800, 600));
+	//glwindow constructed in the frame constructor.  Next line redundant.
+	//theWxFrame->glwindow = new H3DWxWidgetsWindow(theWxFrame);
+	theWxFrame->Show(true);
+
+  if( cmd_line_filename != "" ) theWxFrame->loadFile( string(cmd_line_filename) );
+  //This next line is used to set the icon file h3d.ico, when created.
+	//theWxframe->SetIcon(wxIcon(_T("h3d_icn")));
+
+    
+    // Using this line instead of the two previous lines will make
+    // H3DWxWidgetsWindow create an instance of a wxframe with no menus and use
+    // this as parent to the canvas.
+    // H3DWxWidgetsWindow *glwindow = new H3DWxWidgetsWindow();
+
+  return true;
+}
+
---
+++ branches/daniel_devel/examples/Particledemo/src/emittersettings.h
@@ -0,0 +1,243 @@
+#include <wx/wx.h>
+#include <wx/propdlg.h> 
+#include <wx/bookctrl.h>
+#include <wx/docview.h>
+#include <wx/spinctrl.h>
+#include <wx/config.h>
+#include <wx/confbase.h>
+#include <wx/tokenzr.h>
+
+//#include <SFVec3f.h>
+
+#include "PointEmitter.h"
+#include "ConeEmitter.h"
+#include "ExplosionEmitter.h"
+//#include "PolylineEmitter.h"
+//#include "VolumeEmitter.h"
+#include "ParticleSystem.h"
+#include "SurfaceEmitter.h"
+
+using namespace std;
+using namespace H3D;
+
+// ---------------------------------------------------------------------------
+//  Base Emitter Dialog Declaration
+//  All dialogs are derived from this base class
+// ---------------------------------------------------------------------------
+
+class EmitterDialog: public wxPropertySheetDialog
+{
+DECLARE_CLASS(EmitterDialog);
+public:
+  EmitterDialog ();
+  ~EmitterDialog ();
+
+  void setParticleSystem (ParticleSystem * PS);
+  void setEmitter (X3DParticleEmitterNode * emitter);
+
+protected:
+    enum {
+        ID_SHOW_TOOLTIPS = 100,
+
+        ID_SPEED,
+        ID_VARIATION,
+        ID_MASS,
+        ID_SURFACE_AREA,
+    };
+
+    wxImageList*    m_imageList;
+    ParticleSystem * ePS;
+
+DECLARE_EVENT_TABLE()
+};
+
+// ---------------------------------------------------------------------------
+//  PointEmitter Dialog Declaration
+// ---------------------------------------------------------------------------
+
+class PointEmitterDialog: public EmitterDialog
+{
+DECLARE_CLASS(PointEmitterDialog);
+public:
+  PointEmitterDialog(wxWindow* parent, ParticleSystem* PS);
+  ~PointEmitterDialog();
+
+  void handleSettingsChange (wxCommandEvent & event);
+  wxPanel* CreatePointEmitterSettingsPage(wxWindow* parent);  
+
+protected:
+  enum {
+    ID_DIRECTION,
+    ID_POSITION,
+  };
+  wxImageList*    m_imageList;
+  PointEmitter * pointEmitter;
+  double val;
+
+DECLARE_EVENT_TABLE()
+};
+
+// ---------------------------------------------------------------------------
+//  PolylineEmitter Dialog Declaration
+// ---------------------------------------------------------------------------
+
+class PolylineEmitterDialog: public EmitterDialog
+{
+DECLARE_CLASS(PolylineEmitterDialog)
+public:
+    PolylineEmitterDialog(wxWindow* parent, ParticleSystem* PS);
+    ~PolylineEmitterDialog();
+
+  /*void handleSettingsChange (wxCommandEvent & event);
+  void handleSpinEvent (wxSpinEvent & event);
+  void OnOk (wxCommandEvent & event);
+  void OnCancel (wxCommandEvent & event);*/
+
+  wxPanel* CreatePolylineEmitterSettingsPage(wxWindow* parent);  
+  
+protected:
+
+    enum {
+        ID_COORD_INDEX,
+    };
+
+    wxImageList*    m_imageList;
+//    PolylineEmitter * polylineEmitter;
+//    ParticleSystem * ePS;
+
+DECLARE_EVENT_TABLE()
+};
+
+// ---------------------------------------------------------------------------
+//  VolumeEmitter Dialog Declaration
+// ---------------------------------------------------------------------------
+
+class VolumeEmitterDialog: public EmitterDialog
+{
+DECLARE_CLASS(VolumeEmitterDialog)
+public:
+    VolumeEmitterDialog(wxWindow* parent, ParticleSystem* PS);
+    ~VolumeEmitterDialog();
+
+  /*void handleSettingsChange (wxCommandEvent & event);
+  void handleSpinEvent (wxSpinEvent & event);
+  void OnOk (wxCommandEvent & event);
+  void OnCancel (wxCommandEvent & event);*/
+
+  wxPanel* CreateVolumeEmitterSettingsPage(wxWindow* parent);  
+  
+protected:
+
+    enum {
+        ID_COORD_INDEX,
+    };
+
+    wxImageList*    m_imageList;
+//    VolumeEmitter * volumeEmitter;
+//    ParticleSystem * ePS;
+
+DECLARE_EVENT_TABLE()
+};
+
+// ---------------------------------------------------------------------------
+//  ConeEmitter Dialog Declaration
+// ---------------------------------------------------------------------------
+
+class ConeEmitterDialog: public EmitterDialog
+{
+DECLARE_CLASS(ConeEmitterDialog);
+public:
+  ConeEmitterDialog(wxWindow* parent, ParticleSystem* PS);
+  ~ConeEmitterDialog();
+
+  void handleSettingsChange (wxCommandEvent & event);
+  wxPanel* CreateConeEmitterSettingsPage(wxWindow* parent);  
+
+protected:
+  enum {
+    ID_DIRECTION,
+    ID_POSITION,
+  };
+  wxImageList*    m_imageList;
+  ConeEmitter * coneEmitter;
+  double val;
+
+DECLARE_EVENT_TABLE()
+};
+
+// ---------------------------------------------------------------------------
+//  ExplosionEmitter Dialog Declaration
+// ---------------------------------------------------------------------------
+
+class ExplosionEmitterDialog: public EmitterDialog
+{
+DECLARE_CLASS(ExplosionEmitterDialog);
+public:
+  ExplosionEmitterDialog(wxWindow* parent, ParticleSystem* PS);
+  ~ExplosionEmitterDialog();
+
+  void handleSettingsChange (wxCommandEvent & event);
+  wxPanel* CreateExplosionEmitterSettingsPage(wxWindow* parent);  
+
+protected:
+  enum {
+    ID_POSITION,
+  };
+  wxImageList*    m_imageList;
+  ExplosionEmitter * explosionEmitter;
+  double val;
+
+DECLARE_EVENT_TABLE()
+};
+
+// ---------------------------------------------------------------------------
+//  SurfaceEmitter Dialog Declaration
+// ---------------------------------------------------------------------------
+
+class SurfaceEmitterDialog: public EmitterDialog
+{
+DECLARE_CLASS(SurfaceEmitterDialog);
+public:
+  SurfaceEmitterDialog(wxWindow* parent, ParticleSystem* PS);
+  ~SurfaceEmitterDialog();
+
+  void handleSettingsChange (wxCommandEvent & event);
+  wxPanel* CreateSurfaceEmitterSettingsPage(wxWindow* parent);  
+
+protected:
+  enum {
+    ID_COORD_INDEX,
+    ID_SURFACE,
+  };
+  wxImageList*    m_imageList;
+  SurfaceEmitter * surfaceEmitter;
+  double val;
+
+DECLARE_EVENT_TABLE()
+};
+
+//class SurfaceEmitterGeometryDialog: public wxPropertySheetDialog
+//{
+//DECLARE_CLASS(SurfaceEmitterGeometryDialog);
+//public:
+//  EmitterDialog ();
+//  ~EmitterDialog ();
+//
+//  void setParticleSystem (ParticleSystem * PS);
+//  void setEmitter (X3DParticleEmitterNode * emitter);
+//
+//protected:
+//    enum {
+//        ID_SHOW_TOOLTIPS = 100,
+//
+//        ID_SPEED,
+//        ID_VARIATION,
+//        ID_MASS,
+//        ID_SURFACE_AREA,
+//    };
+//
+//    wxImageList*    m_imageList;
+//    ParticleSystem * ePS;
+//
+//DECLARE_EVENT_TABLE()
+//};
