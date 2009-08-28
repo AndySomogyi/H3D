440fdbf522dc7e650942d765bd54ba416bcc9d9e
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2009-08-28 18:48:34 +0000

    Fixed a bug which produced glNewList errors for X3DShapeNodes when the scene contains transparent objects

--- include/H3D/X3DShapeNode.h
+++ include/H3D/X3DShapeNode.h
@@ -130,6 +130,12 @@ namespace H3D {
     /// By using an intermediate class the bug dissappears.
     class H3DAPI_API BugWorkaroundDisplayList: 
       public H3DDisplayListObject::DisplayList {
+      /// Returns true if we have a valid display list built that can
+      /// be called. We have to override this method here since the 
+      /// render() function of the X3DShapeNode have the possibility
+      /// to break the cache. The display list can then only be considered
+      /// valid if we know that the cache will not be broken. 
+      virtual bool haveValidDisplayList();
     };
 
     /// Display list is extended in 
