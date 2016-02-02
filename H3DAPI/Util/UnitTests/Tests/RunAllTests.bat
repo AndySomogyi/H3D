ca458c7c50df6e522ce3d8cf115fecf369338993
michaels <michaels@610dfd11-2700-0410-9b68-9ae28eac5983>
2016-02-02 13:59:55 +0000

    Added an example bat file for running the tests.

---
+++ H3DAPI/Util/UnitTests/Tests/RunAllTests.bat
@@ -0,0 +1,3 @@
+cd ..
+python.exe RunTests.py --dbhost 127.0.0.1 --dbname <name of the database, default is testserver> --dbuser <username> --dbpass <password> --workingdir <the root directory of the folders containing tests> --servername <the name you want the results to be reported under>
+cd Tests
