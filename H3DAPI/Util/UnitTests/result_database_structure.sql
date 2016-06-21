162db55fc38bd75fb66f2ad5103162e5c8845dbd
michaels <michaels@610dfd11-2700-0410-9b68-9ae28eac5983>
2016-06-21 12:09:11 +0000

    Updated database structure that contains indexes for improved performance.

--- H3DAPI/Util/UnitTests/result_database_structure.sql
+++ H3DAPI/Util/UnitTests/result_database_structure.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.1.9-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
+-- Server version:               5.1.73 - Source distribution
+-- Server OS:                    redhat-linux-gnu
 -- HeidiSQL Version:             9.1.0.4867
 -- --------------------------------------------------------
 
@@ -10,12 +10,12 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping database structure for testserver_new
-CREATE DATABASE IF NOT EXISTS `testserver_new` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `testserver_new`;
+-- Dumping database structure for hms_testdatabase
+CREATE DATABASE IF NOT EXISTS `hms_testdatabase` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `hms_testdatabase`;
 
 
--- Dumping structure for table testserver_new.console_results
+-- Dumping structure for table hms_testdatabase.console_results
 CREATE TABLE IF NOT EXISTS `console_results` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `test_run_id` int(10) unsigned NOT NULL,
@@ -26,13 +26,14 @@ CREATE TABLE IF NOT EXISTS `console_results` (
   `output` text NOT NULL,
   `baseline` text,
   `diff` text,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `test_run_id_success` (`test_run_id`,`success`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.custom_results
+-- Dumping structure for table hms_testdatabase.custom_results
 CREATE TABLE IF NOT EXISTS `custom_results` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `test_run_id` int(10) unsigned NOT NULL,
@@ -43,13 +44,14 @@ CREATE TABLE IF NOT EXISTS `custom_results` (
   `output` text NOT NULL,
   `baseline` text,
   `diff` text,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+  PRIMARY KEY (`id`),
+  KEY `test_run_id_success` (`test_run_id`,`success`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.error_results
+-- Dumping structure for table hms_testdatabase.error_results
 CREATE TABLE IF NOT EXISTS `error_results` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `test_run_id` int(10) unsigned NOT NULL,
@@ -58,13 +60,14 @@ CREATE TABLE IF NOT EXISTS `error_results` (
   `step_id` int(10) unsigned NOT NULL,
   `stdout` text,
   `stderr` text,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `test_run_id` (`test_run_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.performance_results
+-- Dumping structure for table hms_testdatabase.performance_results
 CREATE TABLE IF NOT EXISTS `performance_results` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `test_run_id` int(10) unsigned NOT NULL,
@@ -73,12 +76,12 @@ CREATE TABLE IF NOT EXISTS `performance_results` (
   `step_id` int(10) unsigned NOT NULL,
   `full_profiling_data` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.performance_result_data
+-- Dumping structure for table hms_testdatabase.performance_result_data
 CREATE TABLE IF NOT EXISTS `performance_result_data` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `performance_result_id` int(10) unsigned NOT NULL,
@@ -87,12 +90,12 @@ CREATE TABLE IF NOT EXISTS `performance_result_data` (
   `mean` float NOT NULL,
   `percent` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.rendering_baselines
+-- Dumping structure for table hms_testdatabase.rendering_baselines
 CREATE TABLE IF NOT EXISTS `rendering_baselines` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `file_id` int(10) unsigned NOT NULL,
@@ -100,12 +103,12 @@ CREATE TABLE IF NOT EXISTS `rendering_baselines` (
   `step_id` int(10) unsigned NOT NULL,
   `image` mediumblob NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.rendering_results
+-- Dumping structure for table hms_testdatabase.rendering_results
 CREATE TABLE IF NOT EXISTS `rendering_results` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `test_run_id` int(10) unsigned NOT NULL,
@@ -115,60 +118,61 @@ CREATE TABLE IF NOT EXISTS `rendering_results` (
   `success` enum('Y','N') NOT NULL,
   `output_image` mediumblob,
   `diff_image` mediumblob,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+  PRIMARY KEY (`id`),
+  KEY `test_run_id_success` (`test_run_id`,`success`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.servers
+-- Dumping structure for table hms_testdatabase.servers
 CREATE TABLE IF NOT EXISTS `servers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `server_name` tinytext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.test_cases
+-- Dumping structure for table hms_testdatabase.test_cases
 CREATE TABLE IF NOT EXISTS `test_cases` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `case_name` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.test_files
+-- Dumping structure for table hms_testdatabase.test_files
 CREATE TABLE IF NOT EXISTS `test_files` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `filename` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.test_runs
+-- Dumping structure for table hms_testdatabase.test_runs
 CREATE TABLE IF NOT EXISTS `test_runs` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `timestamp` datetime NOT NULL,
   `server_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
 
--- Dumping structure for table testserver_new.test_steps
+-- Dumping structure for table hms_testdatabase.test_steps
 CREATE TABLE IF NOT EXISTS `test_steps` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `step_name` tinytext NOT NULL,
   `test_case_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
