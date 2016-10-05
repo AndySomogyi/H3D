fa2b84327535b0e59e014e934c5596d81b9cc411
mike <mike@610dfd11-2700-0410-9b68-9ae28eac5983>
2016-10-05 14:11:05 +0000

    Added ability to specify projection width and height for FrameBufferTextureGenerator

--- H3DAPI/include/H3D/FrameBufferTextureGenerator.h
+++ H3DAPI/include/H3D/FrameBufferTextureGenerator.h
@@ -235,6 +235,8 @@ namespace H3D {
                                  Inst< SFInt32          > _height    = 0,
                                  Inst< SFInt32          > _widthInUse = 0,
                                  Inst< SFInt32          > _heightInUse = 0,
+                                 Inst< SFInt32          > _projectionWidth = 0,
+                                 Inst< SFInt32          > _projectionHeight = 0,
                                  Inst< SFBool           > _useStereo = 0,
                                  Inst< SFString         > _depthBufferStorage = 0,
                                  Inst< SFFrameBufferTextureGeneratorNode > _externalFBODepthBuffer = 0,
@@ -532,6 +534,18 @@ namespace H3D {
     /// It contains the actual height value for this node used at realtime
     /// <b>Access type:</b> outputOnly
     auto_ptr< SFInt32 > heightInUse;
+
+    /// Specify the width used to determine the local viewpoint's projection matrix
+    /// if not specified the window's width and height will be used
+    /// <b>Access type:</b> inputOutput
+    /// <b>Default value:</b> -1
+    auto_ptr< SFInt32 > projectionWidth;
+
+    /// Specify the height used to determine the local viewpoint's projection matrix
+    /// if not specified the window's width and height will be used
+    /// <b>Access type:</b> inputOutput
+    /// <b>Default value:</b> -1
+    auto_ptr< SFInt32 > projectionHeight;
 
     /// An option to enable the use of same stereo setting as current window
     /// by default,it will be set to false and MONO will be used for rendering
