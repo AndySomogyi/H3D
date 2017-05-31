d720fd06ca06b2ba8a62c9718dc7286fb6e6244c
michaels <michaels@610dfd11-2700-0410-9b68-9ae28eac5983>
2017-05-31 09:15:59 +0000

    T986: Added functionality for specifying multiple svn paths, for projects where the tests are run with multiple different testdef directories. It will look for every testdef you want to update baselines for in each of the specified directories.

--- trunk/H3DAPI/Util/SGTestFramework/www/mysql_conf.php
+++ trunk/H3DAPI/Util/SGTestFramework/www/mysql_conf.php
@@ -6,7 +6,7 @@ $dbuser = 'ResultReader';
 $dbpass = 'results';
 $svnuser = ''; // Needs to have read and write access to $svnpath.
 $svnpass = ''; 
-$svnpath = ''; // Needs to be a full svn url for checking out the root directory of the tests that are shown on this page. Necessary for baseline uploads.
+$svnpaths = []; // Needs to be a list of full svn urls for checking out the root directory of any of the tests that are shown on this page. Necessary for baseline uploads.
 
 $db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if(mysqli_connect_errno($db)) {
